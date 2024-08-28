09eee4c6c7b744828ca6cc53f0daef15560afa85
Gabriel Varanda <Gabrielluizvaranda@gmail.com>
2024-08-28 12:55:14 +0000

    teste

---
+++ aula03.java
@@ -0,0 +1,16 @@
+class TesteIdade {
+    public static void main(string[] args) {
+        double resultado = idade/2;
+        boolean maiorIdade;
+        maiorIdade = idade >= 18;
+        boolean tipoSangue;
+        tipoSangue = 'a' == 'A';
+        System.out.println(tipoSangue);
+
+        int i = 5;
+        int j = i;
+        i = i + 1;
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
